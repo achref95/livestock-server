a72fc89f7fc09201fbf008d380a972525472fe11
Achref LAABIDI <abidi.achref95@gmail.com>
2023-11-22 17:02:25 +0100

    added auth routes

---
+++ models/LiveStock.model.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required."],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required."],
+    },
+
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
