f9b4295dc3d99bb3647202b922929a4eb4fb8e9b
Achref LAABIDI <abidi.achref95@gmail.com>
2023-12-03 16:17:22 +0100

    routes improvements

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
 const liveStockRoutes = require("./routes/ls.routes");
-app.use("/", liveStockRoutes);
+app.use("/ls", liveStockRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
--- routes/ls.routes.js
+++ routes/ls.routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { isAuthenticated } = require("../middleware/jwt.middleware");
 const { addLs } = require("../controllers/addLsController");
 
-router.post("/addls", addLs);
+router.post("/addls", isAuthenticated, addLs);
 
 module.exports = router;
